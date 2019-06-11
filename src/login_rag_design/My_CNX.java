bbb5b9051f076358028e3a9ca92df5e547fba7fd
mettra chakma <jagatchakmajc@gmail.com>
2019-06-10 17:03:09 -0700

    Frist commit/uplode

---
+++ src/login_rag_design/My_CNX.java
@@ -0,0 +1,37 @@
+package login_rag_design;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class My_CNX {
+    private static String servername = "localhost";
+    private static String username = "root";
+    private static String password = "";
+    private static Integer portnumber = 3306;
+    private static String dbname = "users_db";
+    
+    public static Connection getConnection(){
+        Connection cnx = null;
+        
+        MysqlDataSource datasource = new MysqlDataSource();
+        
+        datasource.setServerName(servername);
+        datasource.setUser(username);
+        datasource.setPassword(password);
+        datasource.setDatabaseName(dbname);
+        datasource.setPortNumber(portnumber);
+        
+        try {
+            cnx = datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(" Get Connection -> "+My_CNX.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        return cnx;
+    }
+    
+}
